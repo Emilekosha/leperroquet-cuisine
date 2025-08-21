45e656b3f3590c1fca1a98600334ab5770d71caa
emilekosha <emilezolagarvane@gmail.com>
2025-08-21 08:44:21 +0000

    modification

--- src/components/AdCard.jsx
+++ src/components/AdCard.jsx
@@ -5,7 +5,7 @@ export default function AdCard() {
   return (
     <Link
       to="/publicite"
-      className="flex flex-col md:flex-row bg-gray-100 rounded-lg overflow-hidden shadow-md mt-4 hover:shadow-lg transition"
+      className="flex flex-col md:flex-row bg-gray-100 rounded-lg overflow-hidden  mt-4 "
     >
       {/* Image à gauche (ou au-dessus sur mobile) */}
       <img
@@ -16,10 +16,9 @@ export default function AdCard() {
 
       {/* Texte à droite (ou en dessous sur mobile) */}
       <div className="p-4 flex flex-col justify-center">
-        <p className="text-sm text-gray-700 font-semibold mb-2 text-center">
-           Sponsorisé par <span className="text-gray-800 font-bold">
-           Terre &amp; Saveurs
-          </span>
+        <p className="text-lg text-gray-700 font-semibold mb-2 text-center">
+          Sponsorisé par{" "}
+          <span className="text-gray-800 font-bold ">Terre &amp; Saveurs</span>
         </p>
         <p className="text-lg font-semibold text-gray-800 leading-snug px-10 text-center">
           Mieux manger au quotidien : quand le sain devient simple (et
